63db872410e908364cfc1b0c1484573c973b9ffe
zhangqingwu <18656007202@163.com>
2021-11-11 21:36:04 +0800

    feat: 🎸 remove unuse

--- src/utils/number.ts
+++ src/utils/number.ts
@@ -1,29 +1,4 @@
 import * as number from "@foxone/utils/number";
-import { shouldPolyfill } from "@formatjs/intl-numberformat/should-polyfill";
-
-async function polyfill(locale: string) {
-  if (!shouldPolyfill(locale)) {
-    return;
-  }
-  // Load the polyfill 1st BEFORE loading data
-  await import("@formatjs/intl-numberformat/polyfill");
-  switch (locale) {
-    default:
-      await import("@formatjs/intl-numberformat/locale-data/en");
-      break;
-    case "fr":
-      await import("@formatjs/intl-numberformat/locale-data/fr");
-      break;
-  }
-}
-
-polyfill("en");
-polyfill("fr");
-polyfill("ja");
-polyfill("de");
-polyfill("es");
-polyfill("zh");
-polyfill("ko");
 
 export function simplize(vm: Vue, opts: { n: number }) {
   const locale = vm.$store.$i18n.locale;
